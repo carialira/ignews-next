f2d418501ce46641938dd942c4ea812b41d4cc9c
Ariadne Candido Liranço <aricandidolira@gmail.com>
2023-11-08 07:33:02 -0300

    Create API

---
+++ src/pages/api/users.tsx
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { use } from "react"
+
+export default (request:NextApiRequest, reponse:NextApiResponse) => {
+  const users = [
+    {id:1, name:'Ari'},
+    {id:2, name:'Gabs'},
+    {id:3, name:'Roze'},
+    {id:4, name:'Be'},
+  ]
+
+  return Response.json(users)
+}
